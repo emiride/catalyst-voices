0d7c4053263c4459d5ae15f46fcd2dd5f44fccf1
digitalheartxs <jakub.szwiec@scalac.io>
2024-09-18 16:48:39 +0200

    feat: Data-Editor behaviour, more content

--- catalyst_voices/lib/pages/workspace/workspace_page.dart
+++ catalyst_voices/lib/pages/workspace/workspace_page.dart
@@ -2,45 +2,105 @@ import 'package:catalyst_voices/pages/workspace/proposal_details.dart';
 import 'package:catalyst_voices/pages/workspace/proposal_navigation_panel.dart';
 import 'package:catalyst_voices/pages/workspace/proposal_segment_controller.dart';
 import 'package:catalyst_voices/pages/workspace/proposal_setup_panel.dart';
-import 'package:catalyst_voices/pages/workspace/sample_rich_text.dart';
-import 'package:catalyst_voices/pages/workspace/sample_rich_text2.dart';
-import 'package:catalyst_voices/pages/workspace/sample_rich_text3.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/answer.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/bonus_mark_up.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/delivery_and_accountability.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/feasibility_checks.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/problem_statement.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/public_description.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/solution_statement.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/title.dart';
+import 'package:catalyst_voices/pages/workspace/rich_text/value_for_money.dart';
 import 'package:catalyst_voices/widgets/widgets.dart';
 import 'package:catalyst_voices_models/catalyst_voices_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
 const _setupSegmentId = 'setup';
+const _summarySegmentId = 'summary';
+const _solutionSegmentId = 'solution';
+const _impactSegmentId = 'impact';
+const _capabilityAndFeasibilitySegmentId = 'capabilityAndFeasibility';
 
-final _proposalNavigation = WorkspaceProposalNavigation(
+var _proposalNavigation = WorkspaceProposalNavigation(
   segments: [
     WorkspaceProposalSetup(
       id: _setupSegmentId,
       steps: [
-        const WorkspaceProposalSegmentStep(
+        WorkspaceProposalSegmentStep(
           id: 0,
           title: 'Title',
-          description: 'F14 / Promote Social Entrepreneurs and a '
-              'longer title up-to 60 characters',
-          isEditable: true,
+          document: Document.fromJson(title),
+        ),
+      ],
+    ),
+    WorkspaceProposalSummary(
+      id: _summarySegmentId,
+      steps: [
+        WorkspaceProposalSegmentStep(
+          id: 0,
+          title: 'Problem statement',
+          document: Document.fromJson(problemStatement),
         ),
         WorkspaceProposalSegmentStep(
           id: 1,
-          title: 'Rich text',
-          document: Document.fromJson(sampleRichText),
-          isEditable: true,
+          title: 'Solution statement',
+          document: Document.fromJson(solutionStatement),
         ),
         WorkspaceProposalSegmentStep(
           id: 2,
-          title: 'Other topic',
-          document: Document.fromJson(sampleRichText2),
-          isEditable: false,
+          title: 'Public description',
+          document: Document.fromJson(publicDescription),
+        ),
+      ],
+    ),
+    WorkspaceProposalSolution(
+      id: _solutionSegmentId,
+      steps: [
+        WorkspaceProposalSegmentStep(
+          id: 0,
+          title: 'Problem perspective',
+          document: Document.fromJson(answer),
         ),
         WorkspaceProposalSegmentStep(
-          id: 3,
-          title: 'Other topic',
-          document: Document.fromJson(sampleRichText3),
-          isEditable: false,
+          id: 1,
+          title: 'Perspective rationale',
+          document: Document.fromJson(answer),
+        ),
+        WorkspaceProposalSegmentStep(
+          id: 2,
+          title: 'Project engagement',
+          document: Document.fromJson(answer),
+        ),
+      ],
+    ),
+    WorkspaceProposalImpact(
+      id: _impactSegmentId,
+      steps: [
+        WorkspaceProposalSegmentStep(
+          id: 0,
+          title: 'Bonus mark-up',
+          document: Document.fromJson(bonusMarkUp),
+        ),
+        WorkspaceProposalSegmentStep(
+          id: 1,
+          title: 'Value for Money',
+          document: Document.fromJson(valueForMoney),
+        ),
+      ],
+    ),
+    WorkspaceProposalCapabilityAndFeasibility(
+      id: _capabilityAndFeasibilitySegmentId,
+      steps: [
+        WorkspaceProposalSegmentStep(
+          id: 0,
+          title: 'Delivery & Accountability',
+          document: Document.fromJson(deliveryAndAccountability),
+        ),
+        WorkspaceProposalSegmentStep(
+          id: 1,
+          title: 'Feasibility checks',
+          document: Document.fromJson(feasibilityChecks),
         ),
       ],
     ),
